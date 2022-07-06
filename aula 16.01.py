75bb9f072988946f650413d5522cf6f04bcf9712
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-07-06 20:46:22 -0300

    confirmar

--- aula 16.01.py
+++ aula 16.01.py
@@ -13,3 +13,7 @@ print('A ordem alfabética dos times do campeonato brasileiro série A é:')
 print(sorted(campeonato))
 print('O time do {} está na posição 7! '.format(campeonato[6]))
 
+# lembrar que se pode manipular tuplas dessa forma:
+# 1 solicitação) {campeonato[0:5]}
+# 2 solicitação) {campeonato[-4:]}
+# 4 solicitação) print(f'o São Paulo está na {campeonato.index("São Paulo")+1} posição')
