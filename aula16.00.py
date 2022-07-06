7ff4058d2cfaea5622133137e7fdc005ba3b99d7
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-07-06 20:46:15 -0300

    confirmar

--- aula16.00.py
+++ aula16.00.py
@@ -7,4 +7,12 @@ for c in range(len(extenso)):
         print(f'Voce digitou o numero {extenso[num]}')
 if num < 0 or num > 20:
     print('Número inválido! ')
-        
+
+# extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze')
+# extenso += ('Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+# while True:
+#       num = int(input('Digite um numero entre 0 e 20:))
+#       if 0 <= num <= 20:
+#           break
+#       print('Tente novamente. ', end = '')
+# print(f'Voce digitou o numero {extenso[num]}')
